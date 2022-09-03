51ff97601fca1aaafac26a72f69fda14969d2f2a
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-03 23:00:04 +1200

    created questions sqlite db

--- server/db/migrations/questions.js
+++ server/db/migrations/questions.js
@@ -14,6 +14,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.dropTable('questions')
-}
+// exports.down = function (knex) {
+//   // return knex.schema.dropTable('questions')
+// }
