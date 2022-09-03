381867910d47482fed807518fb92034b1e0e6862
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-03 22:43:23 +1200

    added questions seeds

--- server/db/seeds/questions.js
+++ server/db/seeds/questions.js
@@ -8,13 +8,71 @@ exports.seed = async function (knex) {
   await knex('questions').insert([
     {
       question_id: 1,
-      question: 'Australia',
+      question:
+        'What is the name of the grape variety that Central Otago produces the most of?',
       is_active: false,
+      //Pinot noir
     },
     {
       question_id: 2,
-      question: 'Australia',
+      question:
+        'What type of wine variety is most commonly grown in the Marlborough Region?',
       is_active: false,
+      //Sav
+    },
+    {
+      question_id: 3,
+      question:
+        'What wine region is known for growing the grape variety Gerwurtztraminer?',
+      is_active: false,
+      //Gisbourne
+    },
+    {
+      question_id: 4,
+      question:
+        'What wine region is known for growing the grape variety Gerwurtztraminer?',
+      is_active: false,
+      //Gisbourne
+    },
+    {
+      question_id: 5,
+      question:
+        'The grape variety Cabernet Sauvignon is most commonly grown in which wine region?',
+      is_active: false,
+      //Hawke's Bay
+    },
+    {
+      question_id: 6,
+      question:
+        'What is the most commonly planted grape variety in New Zealand?',
+      is_active: false,
+      //Sav
+    },
+    {
+      question_id: 7,
+      question:
+        'What was the first wine brand to set up in Malrlborough in 1973?',
+      is_active: false,
+      //Montana
+    },
+    {
+      question_id: 8,
+      question:
+        'What is the main grape variety produced in the Wairarapa Region?',
+      is_active: false,
+      //Pinot Noir
+    },
+    {
+      question_id: 9,
+      question: 'What region was the first vine planted in?',
+      is_active: false,
+      //Northland
+    },
+    {
+      question_id: 10,
+      question: 'What is the main grape variety produced in Northland?',
+      is_active: false,
+      //Chardonnay
     },
   ])
 }
