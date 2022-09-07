e3294f9643f6f25371331745f2f0be05ff4ce478
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-07 23:51:48 +1200

    hit a wall, super close though

--- server/db/db.js
+++ server/db/db.js
@@ -11,11 +11,15 @@ function getOneQuestion(id, db = connection) {
 }
 
 function getQuestionAndAnswers(db = connection) {
-  return db('questions').join(
-    'answers',
-    'answers.answers_id',
-    'answers.question_id'
-  )
+  return db('questions')
+    .join('answers', 'answers.answers_id', 'answers.question_id')
+    .select(
+      'questions.question_id',
+      'questions.is_active',
+      'answers',
+      'answers.right_choice'
+    )
+    .where('questions.question_id', 'answers.question_id')
 }
 
 /**
