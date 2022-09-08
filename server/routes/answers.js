209780b6b0486bc35cb3712e2e8a787d206a6b07
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-08 21:22:42 +1200

    removed answers route

--- server/routes/answers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-// eslint-disable-next-line no-unused-vars
-const db = require('../db/db')
-
-const router = express.Router()
-module.exports = router
--- server/server.js
+++ server/server.js
@@ -7,10 +7,8 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 const questions = require('./routes/questions')
-const answers = require('./routes/answers')
 
 server.use('/api/v1/questions', questions)
-server.use('/api/v1/answers', answers)
 server.use('/v1/*', (req, res) => res.sendStatus(404))
 
 //for any react routes, send to the index.html and the client handle the routing
