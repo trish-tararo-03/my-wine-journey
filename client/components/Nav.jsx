ae764a0a056dca597f97b0cef3d0c8514932e05f
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-09 18:10:46 +1200

    working on styling

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,5 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {
+  Header,
+  StyledNav,
+  NavMenu,
+  NavToggle,
+  MenuItem,
+  StyledLink,
+  Logo,
+} from '../../server/public/styledComponents/Navigation'
+import { FaTimes, FaBars } from 'react-icons/fa'
 
 export default function Nav() {
-  return <div>Nav</div>
+  //setting state for mobile icon
+  const [navOpen, setNavOpen] = useState(false)
+
+  function handleClick() {
+    setNavOpen(!navOpen)
+  }
+
+  return (
+    <Header>
+      <StyledNav>
+        <Logo>
+          <StyledLink to="/Home">MWJ</StyledLink>
+          <NavToggle onClick={handleClick}>
+            <span>{navOpen ? <FaTimes /> : <FaBars />}</span>
+          </NavToggle>
+        </Logo>
+        <NavMenu navOpen={navOpen}>
+          <MenuItem>
+            <StyledLink to="/Learn">Learn</StyledLink>
+          </MenuItem>
+          <MenuItem>
+            <StyledLink to="/Notes">Notes</StyledLink>
+          </MenuItem>
+        </NavMenu>
+      </StyledNav>
+    </Header>
+  )
 }
---
+++ server/public/styledComponents/Navigation.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Header = styled.header`
+  display: flex;
+`
+const StyledNav = styled.nav`
+  border: 2px solid green;
+  width: 100%;
+  height: max-content;
+  position: relative;
+`
+
+const Logo = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1em auto;
+
+  border: 3px solid yellow;
+`
+
+const NavMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  max-height: ${({ navOpen }) => (navOpen ? 'max-content' : '0')};
+  transition: max-height 0.3s ease-in;
+  width: 100%;
+  padding: 0;
+`
+
+const NavToggle = styled.button`
+  cursor: pointer;
+  border: 0;
+  width: 3em;
+  height: 3em;
+  padding: 0em;
+  border-radius: 50%;
+  background-color: transparent;
+  transition: opacity 250ms ease;
+  position: absolute;
+  left: 10px;
+
+  &:hover,
+  &:focus {
+    opacity: 0.75;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`
+
+const MenuItem = styled.li`
+  padding: 1rem 1rem;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  list-style: none;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+  font-weight: 600;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  font-weight: bold;
+  font-size: 1.125rem;
+`
+
+export { Header, StyledNav, NavMenu, NavToggle, MenuItem, StyledLink, Logo }
