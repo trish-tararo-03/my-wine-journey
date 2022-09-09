4fc9a32d673b1ac78cdc5e84c119e36714eab5ac
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-09 12:59:02 +1200

    adding styled components

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import Container from '../../server/public/styledComponents/Container'
+import Row from '../../server/public/styledComponents/Row'
 
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <>
+      <Container>
+        <Row>
+          <h1>My Wine Journey</h1>
+        </Row>
+      </Container>
+    </>
+  )
 }
