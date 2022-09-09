b213d28d88358668f6f6c1a23f8e753227887c5a
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-09 14:39:09 +1200

    added button styled component

---
+++ server/public/styledComponents/Button.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  width: fit-content;
+  padding: 0.25em 1em;
+  border-radius: 10px;
+
+  border: 2px solid palevioletred;
+  color: white;
+`
+
+export default Button
