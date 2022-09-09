89d239df2d21d43be21f936ee9e3ab9cc110d904
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-09 22:50:03 +1200

    fixed export

--- server/public/styledComponents/Title.js
+++ server/public/styledComponents/Title.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1`
+const Title = styled.h1`
   font-size: ${(props) => props.fontSize};
   text-align: center;
   padding: 0.5em 1em;
 `
+export default Title
