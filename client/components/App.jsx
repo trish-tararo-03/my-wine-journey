6daae01ed8fc96de815253736daa41ac376b1dd5
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-09 12:32:03 +1200

    added routes/route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+
 import React from 'react'
 import Home from './Home'
 import Login from './Login'
 import Nav from './Nav'
+import Footer from './Footer'
 
 function App() {
   return (
     <>
       <Nav />
       <Login />
-      <Home />
+      <Routes>
+        <Route path="./Home" element={<Home />}></Route>
+        <Route path="./Login" element={<Login />}></Route>
+      </Routes>
+      <Footer />
     </>
   )
 }
