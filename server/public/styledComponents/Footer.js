564cee044f48afee6ebd943c4cf26a71fef4f154
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-11 22:47:39 +1200

    styled footer

--- server/public/styledComponents/Footer.js
+++ server/public/styledComponents/Footer.js
@@ -1,18 +1,24 @@
 import styled from 'styled-components'
 
 export const StyledFooter = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   width: 100%;
-  height: max-content;
   margin-top: 20px;
-  border: 2px solid green;
+  background-color: #9f0048;
 `
 
 export const Copyright = styled.small`
   text-align: center;
+  margin-left: 3px;
+  font-family: 'Oswald', sans-serif;
+  font-weight: bold;
 `
 export const Wrapper = styled.span`
-  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  margin: 0 auto;
+
+  /* border: 2px solid green; */
 `
